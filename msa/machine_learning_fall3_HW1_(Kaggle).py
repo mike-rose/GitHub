7308de0569f29c52f5a9758c5970b9c2cc329c1e
Mike Rose <unicyclepirate@gmail.com>
2016-11-15 23:48:34 -0500

    some IDs are not in training.csv or test.csv

--- msa/machine_learning_fall3_HW1_(Kaggle).py
+++ msa/machine_learning_fall3_HW1_(Kaggle).py
@@ -6,33 +6,38 @@ Created on Mon Nov 14 14:00:41 2016
 
 explore allstate kaggle data
 """
-
+# %%
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
+# %%
 dataFile = r"C:\Users\mail\Desktop\Files\analytics\machine learning" \
     r"\allState_kaggle\train.csv"
 dat = pd.read_csv(dataFile)
 # id, cat1 - cat116, cont1 - cont14, loss
+# 188319 observations
 
 dataFileTest = r"C:\Users\mail\Desktop\Files\analytics\machine learning" \
     r"\allState_kaggle\test.csv"
 datTest = pd.read_csv(dataFile)
-
-len(dat)
-type(dat)
-
-dat.head(2)
+# 125547 observations
 
 with open(dataFile, "rb") as f:
     temp = f.readlines()
-temp[0]
 
-with open(dataFile, "rb") as f:
-    temp = f.readlines()
-temp[0]
+with open(dataFileTest, "rb") as f:
+    tempT = f.readlines()
+
+# %%
+for i in range(20):
+    print(tempT[i][:10])
 
 for i in range(20):
-    print(temp[i][:10])
+    print(temp[i][:5])
 
+print(len(temp))
+print(len(tempT))
+
+len(temp[3])
+len(tempT[3])
