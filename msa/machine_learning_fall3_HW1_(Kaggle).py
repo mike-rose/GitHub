d308964751ecdbe083c05a0ef92a766259d925ab
Mike Rose <Mike Rose>
2016-11-20 01:56:58 -0500

    mae 1440
    only continuous and binary in random forest

--- msa/machine_learning_fall3_HW1_(Kaggle).py
+++ msa/machine_learning_fall3_HW1_(Kaggle).py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy import stats
 import random
 from sklearn import linear_model, tree
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.metrics import mean_absolute_error as mae
 # %% IMPORT DATA
 path = 'C:/Users/mail/analytics/machinelearning/allState/'
@@ -94,6 +94,10 @@ def Xy(dt):
     y = dt.iloc[:, -1]
     return [X, y]
 
+def score(logy, logyhat):
+    s = mae(np.exp(logy), np.exp(logyhat))
+    return s
+    
 #==============================================================================
 # def ftest(dt):
 #     pass
@@ -107,78 +111,36 @@ def Xy(dt):
 #     return [t,p]
 #==============================================================================
 
-
 # %% =========================================
 # ========= CATEGORICAL EXPLORATION ==========
 # ============================================
 
 #==============================================================================
 # uniqueLevels = [dat.iloc[:, i].unique() for i in range(73, 117)]
-# for u in uniqueLevels:
-#     print(len(u))
+# levelFreqs = [dat.iloc[:, i].value_counts() for i in range(73, 117)]
+# pd.DataFrame(levelFreqs[:10]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[10:20]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[20:28]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[28:34]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[34:38]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[38:41]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[41:43]).plot(kind='bar')
+# pd.DataFrame(levelFreqs[42:44]).plot(kind='bar')
 #==============================================================================
-
-levelFreqs = [dat.iloc[:, i].value_counts() for i in range(73, 117)]
-              
-for l in levelFreqs:
-    print(len(l))
-    pass
-
-# inform multilevel categorical variable binning
-pd.DataFrame(levelFreqs[:10]).plot(kind='bar')
-pd.DataFrame(levelFreqs[10:20]).plot(kind='bar')
-pd.DataFrame(levelFreqs[20:28]).plot(kind='bar')
-pd.DataFrame(levelFreqs[28:34]).plot(kind='bar')
-pd.DataFrame(levelFreqs[34:38]).plot(kind='bar')
-pd.DataFrame(levelFreqs[38:41]).plot(kind='bar')
-pd.DataFrame(levelFreqs[41:43]).plot(kind='bar')
-pd.DataFrame(levelFreqs[42:44]).plot(kind='bar')
 
 # these are helpful for knowing which levels can be grouped
-dat.boxplot(column='loss', by='cat108')
-dat.boxplot(column='loss', by='cat116')
-dat.hist(column='loss', by='cat108')
-dat.hist(column='loss', by='cat113')
-
 #==============================================================================
-# dat.cat113.describe()
-# dat.cat113.value_counts().plot()
-# dat.cat116.value_counts()[100:-200]
-# dat.cat116.value_counts().plot()
+# dat.boxplot(column='loss', by='cat108')
+# dat.boxplot(column='loss', by='cat116')
+# dat.hist(column='loss', by='cat108')
+# dat.hist(column='loss', by='cat113')
 #==============================================================================
-# looking like I categories with fewer than 200 observations \
-# should be grouped or dropped - for all cat vars
 
 #==============================================================================
 # dat.loss.describe()
 # lossMean = 7.6857
 # lossStd = 0.8094
 #==============================================================================
-
-# %%
-# SADLY. NEED TO BOOKMARK THIS CATEGORICAL WORK MAKE A PREDICTION
-#==============================================================================
-# 
-# oneCol = dat.iloc[:, 98]
-# 
-# levels = oneCol.unique()
-# lossByLevel = []
-# tinyLevels = []
-# for level in levels:
-#     # create a Series of the logLoss values for given (variable, level)
-#     oneLevel = dat.loss[oneCol == level]
-# 
-#     if len(oneLevel) < 50:
-#         testRes = stats.ttest_1samp(oneLevel, lossMean)
-#         t = [var, level, len(oneLevel), testRes]
-#         tinyLevels.append(t)
-#     else:
-#         lossByLevel.append(oneLevel)
-# 
-# print(tinyLevels)
-# print(lossByLevel)
-#==============================================================================
-
 
 # %% =========================================
 # ========== CONTINUOUS EXPLORATION ==========
@@ -191,41 +153,18 @@ sns.clustermap(data=corr2, annot=True, fmt='d', cmap='seismic_r')
 
 # %%
 # ============================================
-# ============= LOSS EXPLORATION =============
+# ============= TREES ========================
 # ============================================
+numVars = varList[1:73] + varList[117:]
 
-# dat[dat.loss>20000] # boolean subsetting
+numDat = dat[numVars]
 
-#clf = linear_model.SGDRegressor()
-#clf.fit(dat[con], dat.loss)
-#predictions = clf.predict(testCon)
-#len(testCon)
-#testCon.shape
-#clf.shape
-#clf._get_learning_rate_type
-#clf.predict(testCon)
+pdat = partitionData(numDat)
 
-#############################
+rfr = RandomForestRegressor()
+rfr_m1 = rfr.fit(*Xy(pdat['train']))
+test_X, test_y = Xy(pdat['test'])
 
-# %% =========================================
-# ========= 
-# ============================================
+pred_y = rfr_m1.predict(test_X)
 
-
-
-tree0 = tree.DecisionTreeRegressor()
-tree0 = tree0.fit(trn1)
-pred = tree0.predict(tst0)
-
-# eventually a function to yank off y
-
-X = trn1.loc[:, 'cont1':'cont14']
-y = trn1['loss']
-
-tree1 = tree.DecisionTreeRegressor()
-tree1 = tree1.fit(*t)
-pred1 = tree1.predict(tst1.loc[:, 'cont1':'cont14'])
-pred1
-tst1['loss']
-score = mae(np.exp(tst1['loss']), np.exp(pred1))
-score
+score(test_y, pred_y)
