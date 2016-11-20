1e19f74d56d54310830ab90f460a6dff288ab6b6
Mike Rose <Mike Rose>
2016-11-20 04:07:00 -0500

    stopping point
    need to work in categorical variables and reduce dimensions

--- msa/machine_learning_fall3_HW1_(Kaggle).py
+++ msa/machine_learning_fall3_HW1_(Kaggle).py
@@ -9,77 +9,29 @@ import seaborn as sns
 import numpy as np
 from scipy import stats
 import random
-from sklearn import linear_model, tree
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn import linear_model, tree, svm
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error as mae
 # %% IMPORT DATA
 path = 'C:/Users/mail/analytics/machinelearning/allState/'
 dataFile = path + 'raw/train.csv'
 dat = pd.read_csv(dataFile)
 
-dataFileTest = path + 'raw/test.csv'
-datTest = pd.read_csv(dataFileTest)
-
-# %% ORGANIZE DATA
-# how to slice: dat.loc[:,:] or by index: dat.iloc[:,:]
-
-# transform loss to logloss
-dat.loss = np.log(dat.loss)
 #==============================================================================
-# y = dat.loss
-# y.describe()
-# y.quantile(0.0001)
+# dataFileTest = path + 'raw/test.csv'
+# datTest = pd.read_csv(dataFileTest)
 #==============================================================================
+
+# %% DATA TRANSFORMATION
+dat.loss = np.log1p(dat.loss)
 
 varList = list(dat.columns.values)
-
-#==============================================================================
-# datSort = dat.sort_values('loss')
-# datSort.loss[:40]
-# datSort.loss[-40:]
-# datSort.loss[:100].hist()
-# datSort.loss[-100:].hist()
-#==============================================================================
-# cutting off extreme values: 3 < log(loss) < 11
-
-dat = dat[dat['loss'] > 4.3] # chop 31 smallest (loss) obs
-dat = dat[dat['loss'] < 10.7] # chop 15 largest (loss) obs
-
-meanLoss=np.mean(dat.loss)
 
 # A's and B's to 1's and 0's
 dat[varList[1:73]] = dat[varList[1:73]].replace('A', 1)
 dat[varList[1:73]] = dat[varList[1:73]].replace('B', 0)
 
-twoLetters = ['cat109', 'cat110', 'cat112', 'cat113', 'cat116']
-
-ones = [x for x in varList if x not in twoLetters]            
-# Lower-case two letter values 
-# Put them in order after singltons
-
-for col in twoLetters:
-    srs = list(dat[col])
-    for i in range(len(srs)):
-        if len(srs[i]) == 2:
-            srs[i] = srs[i].lower()
-    dat[col] = srs
- 
-#==============================================================================
-# %% SUBSET DATA
-#==============================================================================
-# sub0 = dat.sample(n=100, random_state=8)
-# trn0 = sub0.sample(frac=0.7, random_state=8)
-# tst0 = sub0.drop(trn0.index)
-# 
-# sub1 = dat.sample(n=1000, random_state=8)
-# trn1 = sub1.sample(frac=0.7, random_state=8)
-# tst1 = sub1.drop(trn1.index)
-# 
-# sub2 = dat.sample(n=10000, random_state=8)
-# trn2 = sub2.sample(frac=0.7, random_state=8)
-# tst2 = sub2.drop(trn2.index)
-#==============================================================================
-
+# %% DEFINE FUNCTIONS
 def partitionData(dt, n=0, ptrain=0.7):
     if n == 0:
         n = len(dt)
@@ -95,7 +47,7 @@ def Xy(dt):
     return [X, y]
 
 def score(logy, logyhat):
-    s = mae(np.exp(logy), np.exp(logyhat))
+    s = mae(np.expm1(logy), np.expm1(logyhat))
     return s
     
 #==============================================================================
@@ -113,8 +65,19 @@ def score(logy, logyhat):
 
 # %% =========================================
 # ========= CATEGORICAL EXPLORATION ==========
-# ============================================
-
+#=============================================
+#==============================================================================
+# datSort = dat.sort_values('loss')
+# dat.loss.hist(bins=12000)
+#==============================================================================
+#==============================================================================
+# for col in twoLetters:
+#     srs = list(dat[col])
+#     for i in range(len(srs)):
+#         if len(srs[i]) == 2:
+#             srs[i] = srs[i].lower()
+#     dat[col] = srs
+#==============================================================================
 #==============================================================================
 # uniqueLevels = [dat.iloc[:, i].unique() for i in range(73, 117)]
 # levelFreqs = [dat.iloc[:, i].value_counts() for i in range(73, 117)]
@@ -138,8 +101,13 @@ def score(logy, logyhat):
 
 #==============================================================================
 # dat.loss.describe()
-# lossMean = 7.6857
-# lossStd = 0.8094
+# meanLoss=np.mean(dat.loss)
+#==============================================================================
+
+#==============================================================================
+# five80 = dat[dat.loss == 580]
+# five80.shape
+# five80.describe()  # cat4 = 1 94%
 #==============================================================================
 
 # %% =========================================
@@ -147,24 +115,43 @@ def score(logy, logyhat):
 # ============================================
 
 # seaborn's clustermap of correlations
-corr = dat.corr()
-corr2 = corr.mul(100).astype(int)
-sns.clustermap(data=corr2, annot=True, fmt='d', cmap='seismic_r')
+#==============================================================================
+# corr = dat[varList[117:]].corr()
+# corr2 = corr.mul(100).astype(int)
+# sns.clustermap(data=corr2, annot=True, fmt='d', cmap='seismic_r')
+#==============================================================================
 
 # %%
 # ============================================
-# ============= TREES ========================
 # ============================================
-numVars = varList[1:73] + varList[117:]
+# ============================================
 
+numVars = varList[0:73] + varList[117:]
 numDat = dat[numVars]
-
 pdat = partitionData(numDat)
 
+# ======= RANDOM FOREST ==============
 rfr = RandomForestRegressor()
 rfr_m1 = rfr.fit(*Xy(pdat['train']))
 test_X, test_y = Xy(pdat['test'])
 
 pred_y = rfr_m1.predict(test_X)
+print(score(test_y, pred_y))
 
-score(test_y, pred_y)
+
+# ================== SVM =================
+# needs major dimension reduction first
+#==============================================================================
+# pdat = partitionData(dat[varList[117:]])
+# svm1 = svm.SVR()
+# svm1.fit(*Xy(pdat['train'])) 
+# test_X, test_y = Xy(pdat['test'])
+# pred_y = svm1.predict(test_X)
+# print(score(test_y, pred_y))
+#==============================================================================
+
+# ================= GRADIENT BOOSTING ===============
+
+
+# ============== NEURAL NETWORKS ====================
+
