ddf4dcff8201c21b6410ac7fdd1b1df5728bac1c
Mike Rose <Mike Rose>
2016-11-19 21:02:12 -0500

    I have a (terrible) MAE

--- msa/machine_learning_fall3_HW1_(Kaggle).py
+++ msa/machine_learning_fall3_HW1_(Kaggle).py
@@ -11,7 +11,7 @@ from scipy import stats
 import random
 from sklearn import linear_model, tree
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.metrics import mean_absolute_error as mae
 # %% IMPORT DATA
 path = 'C:/Users/mail/analytics/machinelearning/allState/'
 dataFile = path + 'raw/train.csv'
@@ -31,6 +31,8 @@ dat.loss = np.log(dat.loss)
 # y.quantile(0.0001)
 #==============================================================================
 
+varList = list(dat.columns.values)
+
 #==============================================================================
 # datSort = dat.sort_values('loss')
 # datSort.loss[:40]
@@ -43,12 +45,18 @@ dat.loss = np.log(dat.loss)
 dat = dat[dat['loss'] > 4.3] # chop 31 smallest (loss) obs
 dat = dat[dat['loss'] < 10.7] # chop 15 largest (loss) obs
 
-varList = list(dat.columns.values)
-catList = range(1, 117)  # exclude ID
-conList = range(117, 131)  # exclude loss
-binaryList = range(1, 73)
-notBinaryList = range(73, 117)
+# A's and B's to 1's and 0's
+dat[varList[1:73]] = dat[varList[1:73]].replace('A', 1)
+dat[varList[1:73]] = dat[varList[1:73]].replace('B', 0)
 
+
+#==============================================================================
+# catList = range(1, 117)  # exclude ID
+# conList = range(117, 131)  # exclude loss
+# binaryList = range(1, 73)
+# notBinaryList = range(73, 117)
+# 
+#==============================================================================
 # %% SUBSET DATA
 sub0 = dat.sample(n=100, random_state=8)
 trn0 = sub0.sample(frac=0.7, random_state=8)
@@ -66,60 +74,57 @@ tst2 = sub2.drop(trn2.index)
 # ========= CATEGORICAL EXPLORATION ==========
 # ============================================
 
-# these are helpful for knowing which levels can be grouped
-dat.boxplot(column='loss', by='cat108')
-dat.boxplot(column='loss', by='cat110')
-dat.hist(column='loss', by='cat108')
-dat.hist(column='loss', by='cat113')
+# which cats are only one letter?
 
-dat.cat113.describe()
-dat.cat113.value_counts().plot()
-dat.cat116.value_counts()[100:-200]
-dat.cat116.value_counts().plot()
+# these are helpful for knowing which levels can be grouped
+#==============================================================================
+# dat.boxplot(column='loss', by='cat108')
+# dat.boxplot(column='loss', by='cat100')
+# dat.hist(column='loss', by='cat108')
+# dat.hist(column='loss', by='cat113')
+#==============================================================================
+
+#==============================================================================
+# dat.cat113.describe()
+# dat.cat113.value_counts().plot()
+# dat.cat116.value_counts()[100:-200]
+# dat.cat116.value_counts().plot()
+#==============================================================================
 # looking like I categories with fewer than 200 observations \
 # should be grouped or dropped - for all cat vars
-dat.loc[:,'cat72':'cat116'].value_counts().plot()
-nb = varlst[73:117]  # non-binary cat var names
 
-# ONE WAY ANOVA -- CHECK FOR DIFFERENCES ACROSS LEVELS OF EACH CATEGORY VAR
-
-
-np.mean(v.sample(frac=0.01))
-varList = list(dat.columns.values)
 dat.loss.describe()
 lossMean = 7.6857
 lossStd = 0.8094
 
 # %%
 # SADLY. NEED TO BOOKMARK THIS CATEGORICAL WORK MAKE A PREDICTION
+#==============================================================================
+# 
+# oneCol = dat.iloc[:, 98]
+# 
+# levels = oneCol.unique()
+# lossByLevel = []
+# tinyLevels = []
+# for level in levels:
+#     # create a Series of the logLoss values for given (variable, level)
+#     oneLevel = dat.loss[oneCol == level]
+# 
+#     if len(oneLevel) < 50:
+#         testRes = stats.ttest_1samp(oneLevel, lossMean)
+#         t = [var, level, len(oneLevel), testRes]
+#         tinyLevels.append(t)
+#     else:
+#         lossByLevel.append(oneLevel)
+# 
+# print(tinyLevels)
+# print(lossByLevel)
+#==============================================================================
 
-oneCol = dat.iloc[:, 98]
-
-levels = oneCol.unique()
-lossByLevel = []
-tinyLevels = []
-for level in levels:
-    # create a Series of the logLoss values for given (variable, level)
-    oneLevel = dat.loss[oneCol == level]
-
-    if len(oneLevel) < 50:
-        testRes = stats.ttest_1samp(oneLevel, lossMean)
-        t = [var, level, len(oneLevel), testRes]
-        tinyLevels.append(t)
-    else:
-        lossByLevel.append(oneLevel)
-
-print(tinyLevels)
-print(lossByLevel)
-
-
-#take 100 random samples of size whatever, use their means in ttest
 
 # %% =========================================
 # ========== CONTINUOUS EXPLORATION ==========
 # ============================================
-dat[con].plot(kind='box', title="distributions of continuous variables")
-dat[con].hist()
 
 # seaborn's clustermap of correlations
 corr = dat.corr()
@@ -130,15 +135,7 @@ sns.clustermap(data=corr2, annot=True, fmt='d', cmap='seismic_r')
 # ============================================
 # ============= LOSS EXPLORATION =============
 # ============================================
-y = dat['loss']
-y.plot.hist(bins=1000)
-ly = np.log(y)
-ly.hist(bins=1000)
-backAgain = np.exp(ly)
-backAgain == y
-backAgain.plot.hist(bins=1000)
-diff = y - backAgain
-diff
+
 # dat[dat.loss>20000] # boolean subsetting
 
 #clf = linear_model.SGDRegressor()
@@ -156,6 +153,18 @@ diff
 # ========= 
 # ============================================
 tree0 = tree.DecisionTreeRegressor()
-tree0 = tree0.fit(trn0, trn0.loss)
+tree0 = tree0.fit(trn1)
 pred = tree0.predict(tst0)
 
+# eventually a function to yank off y
+
+X = trn1.loc[:, 'cont1':'cont14']
+y = trn1['loss']
+
+tree1 = tree.DecisionTreeRegressor()
+tree1 = tree1.fit(X, y)
+pred1 = tree1.predict(tst1.loc[:, 'cont1':'cont14'])
+pred1
+tst1['loss']
+score = mae(np.exp(tst1['loss']), np.exp(pred1))
+score
